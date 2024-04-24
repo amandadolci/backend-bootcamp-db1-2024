eb13b0bf9c79b9315f35b8c4791d6fb9243b798f
Amanda Dolci <amanda.dolci@gmail.com>
2024-04-24 19:23:26 -0300

    feat: progresso aula 1

--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,19 @@ const { ValidationError } = require('sequelize');
 
 const { validationResultCheck } = require('../validators');
 const { validateCreateUser, validateLogin } = require('../validators/users');
+const User = require("../models/User");
 
 const router = express.Router();
 
 function isUniqueEmailError(error) {
-  if (!(error instanceof ValidationError)) {
-    return false;
-  }
+	if (!(error instanceof ValidationError)) {
+		return false;
+	}
 
-  return error.errors.find((databaseError) => (
-    databaseError.type === 'unique violation' && databaseError.path === 'email'
-  ));
+	return error.errors.find(
+		databaseError =>
+			databaseError.type === 'unique violation' && databaseError.path === 'users_email_unique'
+	);
 }
 
 /**
@@ -30,11 +32,15 @@ router.post(
     try {
       const { name, email, password } = req.body;
 
+      let user = await User.create({ name, email, password });	
+      user = await User.findByPk(user.id);
+
       // TODO: implementar aqui
+      res.status(201).send();
     } catch (error) {
       console.warn(error);
       if (isUniqueEmailError(error)) {
-        res.status(402).send('E-mail já cadastrado!');
+        res.status(409).send('E-mail já cadastrado!');
         return;
       }
       res.status(500).send();
--- validators/users.js
+++ validators/users.js
@@ -1,20 +1,55 @@
 const { checkSchema } = require('express-validator');
 
 const validateLogin = checkSchema(
-  {
-    // TODO: implementar validação
-  },
-  ['body'],
+	{
+		// TODO: implementar validação
+	},
+	['body']
 );
 
 const validateCreateUser = checkSchema(
-  {
-    // TODO: implementar validação
-  },
-  ['body'],
+	{
+		name: {
+			isString: {
+				errorMessage: 'O nome deve ser uma string.',
+			},
+			isLength: {
+				options: { max: 200, min: 1 },
+				errorMessage: 'O nome deve ter no mínimo 1 e no máximo 200 caracteres.',
+			},
+			notEmpty: {
+				errorMessage: 'O nome é requerido.',
+			},
+		},
+		email: {
+			isEmail: {
+				errorMessage: 'O email deve ser um endereço de e-mail válido.',
+			},
+			isLength: {
+				options: { max: 200, min: 1 },
+				errorMessage: 'O email deve ter no mínimo 1 e no máximo 200 caracteres.',
+			},
+			notEmpty: {
+				errorMessage: 'O email é requerido.',
+			},
+		},
+		password: {
+			isString: {
+				errorMessage: 'O password deve ser uma string.',
+			},
+			isLength: {
+				options: { max: 20, min: 8 },
+				errorMessage: 'O password deve ter no mínimo 8 e no máximo 20 caracteres.',
+			},
+			notEmpty: {
+				errorMessage: 'O password é requerido.',
+			},
+		},
+	},
+	['body']
 );
 
 module.exports = {
-  validateLogin,
-  validateCreateUser,
+	validateLogin,
+	validateCreateUser,
 };
